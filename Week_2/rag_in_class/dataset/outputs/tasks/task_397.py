42ac725008709bab71cd92836f6223929b43922c
imran <imranraad@gmail.com>
2026-02-10 02:14:34 -0600

    updated

--- Week_2/rag_in_class/dataset/outputs/tasks/task_397.py
+++
@@ -1,7 +0,0 @@
-def median_numbers(a, b, c):
-    """Return the median of three numbers a, b, and c."""
-    if (a <= b <= c) or (c <= b <= a):
-        return b
-    if (b <= a <= c) or (c <= a <= b):
-        return a
-    return c
